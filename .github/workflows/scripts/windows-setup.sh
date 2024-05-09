4f2888866747ceec8c5b3b34e1172df7d7116254
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-09 17:33:03 +0200

    Update windows-setup.sh
    used /

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -2,9 +2,9 @@
 
 # Clone and bootstrap vcpkg
 git clone https://github.com/microsoft/vcpkg
-.\vcpkg\bootstrap-vcpkg.bat
-.\vcpkg\vcpkg integrate install
+./vcpkg/bootstrap-vcpkg.bat
+./vcpkg/vcpkg integrate install
 
-# Install necessary libraries
-.\vcpkg\vcpkg install eigen3
-.\vcpkg\vcpkg install ipopt
+# Install necessary libraries with vcpkg
+./vcpkg/vcpkg install eigen3
+./vcpkg/vcpkg install ipopt
