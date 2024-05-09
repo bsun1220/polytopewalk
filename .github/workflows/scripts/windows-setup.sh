906eddabc89a6c464e1a154fe30f43dad8df613b
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-09 17:27:53 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install Chocolatey and Git
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install git
-
 # Clone and bootstrap vcpkg
 git clone https://github.com/microsoft/vcpkg
 .\vcpkg\bootstrap-vcpkg.bat
