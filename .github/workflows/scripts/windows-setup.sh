52e9f6a85e320c31d8871afccc31ca9c1ba97594
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-09 17:38:42 +0200

    Update windows-setup.sh
    added ifopt source install

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -5,6 +5,13 @@ git clone https://github.com/microsoft/vcpkg
 ./vcpkg/bootstrap-vcpkg.bat
 ./vcpkg/vcpkg integrate install
 
-# Install necessary libraries with vcpkg
+# Install eigen3 and ipopt with vcpkg
 ./vcpkg/vcpkg install eigen3
 ./vcpkg/vcpkg install coin-or-ipopt
+
+# Install ifopt from source
+git clone https://github.com/ethz-adrl/ifopt.git && cd ifopt
+mkdir build && cd build
+cmake ..
+make
+make install
