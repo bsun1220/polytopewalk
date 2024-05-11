cd50fc3818e63c80404446b871eb9d3a7e83b47d
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-11 02:13:45 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -10,16 +10,17 @@ git clone https://github.com/microsoft/vcpkg
 # ./vcpkg/vcpkg install coin-or-ipopt
 
 # Test ipopt install from source
-svn co https://projects.coin-or.org/svn/Ipopt/stable/3.11 CoinIpopt
-cd CoinIpopt/ThirdParty
-cd Blas && ./get.Blas && cd ..
-cd Lapack && ./get.Lapack && cd ..
-cd Metis && ./get.Metis && cd .. (Graph Coloring tool used by e.g. Mumps)
-cd Mumps && ./get.Mumps && cd .. (Sparse direct linear solver with permissive license)
-cd ..; mkdir build; cd build
-../configure --prefix=/usr/local ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC
-make; sudo make install
-pkg-config --libs ipopt
+# Install Mumps
+git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git && cd ThirdParty-Mumps
+./get.Mumps
+./configure
+make; make install
+cd ..
+
+git clone https://github.com/coin-or/Ipopt.git && cd Ipopt
+../configure
+make; make test; make install
+cd ..
 
 # Install ifopt from source
 git clone https://github.com/ethz-adrl/ifopt.git && cd ifopt
