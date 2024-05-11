847bd5d7ad0e8a56d851bc6ff99a615b7376f6a0
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-11 03:59:46 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-choco install strawberryperl
-
 # Clone and bootstrap vcpkg
 git clone https://github.com/microsoft/vcpkg
 ./vcpkg/bootstrap-vcpkg.bat
@@ -11,21 +9,14 @@ git clone https://github.com/microsoft/vcpkg
 ./vcpkg/vcpkg install eigen3
 ./vcpkg/vcpkg install coin-or-ipopt
 
-# Install Ipopt lib from source
-git clone https://github.com/coin-or/Ipopt.git
-cd Ipopt
-./configure
-make
-make test
-make install
-cd ..
-
 # Install ifopt from source
 git clone https://github.com/ethz-adrl/ifopt.git
 cd ifopt
 mkdir build
 cd build
 # handles the CMAKE_PREFIX_PATH for the packages installed via vcpkg
-cmake --trace-expand -S .. -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake
-make
+cmake -S .. \
+  -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" \
+  -DCMAKE_INCLUDE_PATH="../../vcpkg/installed/x64-windows/include" \
+  -DCMAKE_LIBRARY_PATH="../../vcpkg/installed/x64-windows/lib"
 make install
