964c7cc2599319c1454cfc18aee5e8672d4c5f75
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-11 14:44:15 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # install prerequisite
-pacman -Syu
+# pacman -Syu
 # pacman -S binutils diffutils git grep make patch pkg-config
 # pacman -S mingw-w64-x86_64-lapack
 # pacman -S mingw-w64-x86_64-metis
 # pacman -S mingw-w64-x86_64-eigen3
 
-pacman -Qs eigen3
-pacman -Ql eigen3 | grep bin/
+# pacman -Qs eigen3
+# pacman -Ql eigen3 | grep bin/
+
+ls "C:\msys64"
+ls "C:\msys64\mingw64"
+ls "C:\msys64\mingw64\bin"
 
 
 echo "OK pause"
