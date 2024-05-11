e46def54351aac24b5c439e43cc5654cd1efee37
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-11 14:21:27 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -8,6 +8,8 @@ pacman -S mingw-w64-x86_64-lapack
 pacman -S mingw-w64-x86_64-metis
 pacman -S mingw-w64-x86_64-eigen3
 
+pacman -Qs eigen3
+
 echo "OK pause"
 
 git clone https://github.com/ethz-adrl/ifopt.git
