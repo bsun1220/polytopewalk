073bbea379a0f5d61a6da1d003579d7ea81b99ba
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-11 13:38:37 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -2,8 +2,9 @@
 
 # install prerequisite
 pacman -Syu
-pacman -S binutils diffutils git grep make patch pkg-config
-pacman -S mingw-w64-gcc mingw-w64-lfortran
-pacman -S mingw-w64-lapack mingw-w64-metis
+# pacman -S binutils diffutils git grep make patch pkg-config
+pacman -S git
+pacman -S mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-fortran
+pacman -S mingw-w64-x86_64-lapack mingw-w64-x86_64-metis
 
 
