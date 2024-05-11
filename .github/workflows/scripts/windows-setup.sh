6fda79ed314a7f3d6c69a21fa7f283af2445ff11
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-12 01:19:11 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -5,11 +5,19 @@ git clone https://github.com/microsoft/vcpkg
 ./vcpkg/bootstrap-vcpkg.bat
 ./vcpkg/vcpkg integrate install
 
+cd vcpkg
+# Set the VCPKG_ROOT environment variable to the current directory
+export VCPKG_ROOT=($pwd)
+# Display VCPKG_ROOT to verify it is set correctly
+echo "VCPKG_ROOT is set to: $VCPKG_ROOT"
+cd ..
+
+
 # Install eigen3 and ipopt with vcpkg
 ./vcpkg/vcpkg install eigen3
 ./vcpkg/vcpkg install coin-or-ipopt
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./vcpkg/installed/x64-windows/lib/ipopt.lib"
+# export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./vcpkg/installed/x64-windows/lib/ipopt.lib"
 
 echo `ls ./vcpkg/`
 echo `ls ./vcpkg/installed`
@@ -29,9 +37,9 @@ cp ../casadi/cmake/canonicalize_paths.cmake ifopt_ipopt/cmake/
 mkdir build
 cd build
 cmake .. \
-  -DCMAKE_TOOLCHAIN_FILE="../../vcpkg/scripts/buildsystems/vcpkg.cmake" \
-  -DIPOPT_LIBRARIES="../../vcpkg/installed/x64-windows/lib/ipopt.lib" \
-  -DIPOPT_INCLUDE_DIRS="../../vcpkg/installed/x64-windows/include/coin-or/" \
+  -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" \
+  -DIPOPT_LIBRARIES="$VCPKG_ROOT/installed/x64-windows/lib/ipopt.lib" \
+  -DIPOPT_INCLUDE_DIRS="$VCPKG_ROOT/installed/x64-windows/include/coin-or/" \
   -G "Unix Makefiles"
 
 make
