86d0dbe662f3d83e7a63ed5521cdf36b3ab581bd
Yuansi Chen <yuansi.chen1990@gmail.com>
2024-05-11 14:31:11 +0200

    Update windows-setup.sh

--- .github/workflows/scripts/windows-setup.sh
+++ .github/workflows/scripts/windows-setup.sh
@@ -3,19 +3,17 @@
 # install prerequisite
 pacman -Syu
 # pacman -S binutils diffutils git grep make patch pkg-config
-pacman -S git
 pacman -S mingw-w64-x86_64-lapack
 pacman -S mingw-w64-x86_64-metis
 pacman -S mingw-w64-x86_64-eigen3
 
-pacman -Qs git
-pacman -Ql git | grep bin/
-pacman -S git
+pacman -Qs eigen3
+pacman -Ql eigen3 | grep bin/
 
 
 echo "OK pause"
 
-git clone https://github.com/ethz-adrl/ifopt.git
+C:\Program Files\Git\bin\git.exe clone https://github.com/ethz-adrl/ifopt.git
 cd ifopt
 mkdir build
 cd build
